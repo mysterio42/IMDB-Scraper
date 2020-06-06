b1f8ec14ca631bb6ac7330e1e6533bf211e85fbe
mysterio42 <bachana3435@gmail.com>
2020-06-06 14:13:52 +0400

    process item

--- imdb/pipelines.py
+++ imdb/pipelines.py
@@ -24,5 +24,5 @@ class MongodbPipeline:
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.collection_name].insert(dict(item))
+        self.db[self.collection_name].insert_one(dict(item))
         return item
